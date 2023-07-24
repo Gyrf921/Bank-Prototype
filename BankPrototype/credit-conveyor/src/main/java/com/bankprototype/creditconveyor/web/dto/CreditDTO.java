3ad8332e8ce260be00bf1b49892e0cef1a44f107
Oladushek <gyrf921@gmail.com>
2023-07-24 23:16:04 +0300

    I did it beautifully, but it was possible in a different way, but it's not a fact that it would be better

--- BankPrototype/credit-conveyor/src/main/java/com/bankprototype/creditconveyor/web/dto/CreditDTO.java
+++ BankPrototype/credit-conveyor/src/main/java/com/bankprototype/creditconveyor/web/dto/CreditDTO.java
@@ -1,16 +1,10 @@
 package com.bankprototype.creditconveyor.web.dto;
 
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Min;
 import java.math.BigDecimal;
 import java.util.List;
 
 public class CreditDTO {
-
-
     private BigDecimal amount;
-
-
     private Integer term;
     private BigDecimal monthlyPayment;
     private BigDecimal rate;
